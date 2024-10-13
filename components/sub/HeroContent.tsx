74ef522c5fe646e57c1ec6b4e355ec0dd8e7ec16
mando_christ <mandochrist13@gmail.com>
2024-10-13 15:27:57 +0100

    fix text hero

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -27,7 +27,7 @@ const HeroContent = () => {
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="md:text-[#ba9cff] text-slate-800 text-[13px]">
+          <h1 className="md:text-[#ba9cff] font-bold text-slate-800 text-[13px]">
             Développeur web Front-End
           </h1>
         </motion.div>
