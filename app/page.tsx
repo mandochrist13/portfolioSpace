965bf1d5474630859cff6476605215632b7fc5ca
mando_christ <mandochrist13@gmail.com>
2024-10-13 10:25:36 +0100

    mon portfolio est un banger

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
+"use client";
+
 import Encryption from "@/components/main/Encryption";
 import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
-import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  // Ajout de l'audioRef pour lire la musique
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.play().catch((error) => {
+        // Gestion d'erreur si la lecture automatique est bloquée
+        console.log("Lecture automatique bloquée :", error);
+      });
+    }
+  }, []);
+
   return (
     <main className="h-full w-full">
+      {/* L'audio est défini ici et sera lu automatiquement si permis */}
+      <audio
+        ref={audioRef}
+        src="/House of The Dragon_ Targaryen Theme _ EPIC VERSION (Game of Thrones x House of the Dragon).mp3"
+        preload="auto"
+        loop
+      />
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
