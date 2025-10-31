bde1e33cce15080572542fb5eec33603e3df4c96
mando_christ <mandochrist13@gmail.com>
2025-10-31 04:21:23 +0100

    h

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -41,7 +41,7 @@ const HeroContent = () => {
             MANDO
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-             Hamidou Christ-Of-Fair{" "}
+             Christ-Of-Fair{" "}
             </span>
             
           </span>
